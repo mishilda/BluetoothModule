db2985c970a4160432969bf27f337adf071eb488
mishilda <pavlovamar96@yandex.ru>
2023-04-27 12:14:07 +0300

    layouts

--- bluetooth/src/main/java/com/mishilda/bluetooth/BaseActivity.kt
+++ bluetooth/src/main/java/com/mishilda/bluetooth/BaseActivity.kt
@@ -8,8 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_list)
-        initRcView()
+        setContentView(R.layout.activity_base)
+//        initRcView()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.placeHolder, DeviceListFragment()).commit()
     }
 
     private fun initRcView() {
